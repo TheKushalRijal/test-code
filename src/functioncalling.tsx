dbf3fcd76a399f36efae1b620131f30f6834ee70
Kushal Rijal <165631331+TheKushalRijal@users.noreply.github.com>
2026-01-11 16:40:27 -0600

    test 2

--- src/functioncalling.tsx
+++ src/functioncalling.tsx
@@ -1,49 +1,39 @@
 import { runLoginTests } from "./alltestcode/login";
 import { runSigninTests } from "./alltestcode/signup";
-import {
-  runGetCsDataTests,
-  runCreateCsTests,
-  runJoinUserTests,
-} from "./alltestcode/checkcs";
-import { runGetProfileTests } from "./alltestcode/loaduserdata";
-import { runUploadImageTests } from "./alltestcode/uploadimage";
-import { runSearchUsersTests } from "./alltestcode/chats/searchuser";
-import { runSendFriendRequestTests } from "./alltestcode/chats/checkfriends";
-const BASE_URL = "http://192.168.1.153:8080"; // ONE place only
+import { runCreateCsTests } from "./alltestcode/cs_matchcard";
+import { runDeleteCardTests } from "./alltestcode/cs_matchcard";
+import { TEST_TOKENS } from "./tokens";
+
+const BASE_URL = "http://192.168.1.153:8080";
 
 export async function runAllTestsForUI() {
-  const results = [];
+  const results: any[] = [];
 
-  const [
-    loginResults,
-    signinResults,
-    profileResults,
-    createCsResults,
-    joinUserResults,
-    uploadImageResults,
-    usersearch,
-    friendrequest,
-  ] = await Promise.all([
+  // run non-auth tests once
+  const [loginResults, signinResults] = await Promise.all([
     runLoginTests(BASE_URL),
     runSigninTests(BASE_URL),
-    runGetProfileTests(BASE_URL),
-    runCreateCsTests(BASE_URL),
-    runJoinUserTests(BASE_URL),
-    runUploadImageTests(BASE_URL),
-    runSearchUsersTests(BASE_URL),
-   
   ]);
 
-  results.push(
-    ...loginResults,
-    ...signinResults,
-    ...profileResults,
-    ...createCsResults,
-    ...joinUserResults,
-    ...uploadImageResults,
-    ...usersearch,
-   
-  );
+  results.push(...loginResults, ...signinResults);
+
+  // run create → delete per token
+  for (const token of TEST_TOKENS) {
+    const { results: createResults, createdMatchIds } =
+      await runCreateCsTests(BASE_URL, token);
+
+    results.push(...createResults);
+    console.log("this is match id",createdMatchIds);
+    if (createdMatchIds.length > 0) {
+      const deleteResults = await runDeleteCardTests(
+        BASE_URL,
+        token,
+        createdMatchIds
+      );
+
+      results.push(...deleteResults);
+    }
+  }
 
   return results;
 }
